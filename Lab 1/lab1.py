27b838d49a71cd89a17c1d6323be914765862161
R-Eben <eben.radocchia@gmail.com>
2026-09-14 16:43:15 -0400

    added first part of lab1

---
+++ Lab 1/lab1.py
@@ -0,0 +1,9 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+print("Hello, EE 3150!")
+v = np.array([1.0, 2.0, 3.0])
+print("v^2 =", v**2)
+# prints to the console
+# a numpy array
+# vectorized: applies to every elemen
