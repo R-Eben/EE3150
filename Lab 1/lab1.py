7af3112e2fcc1e16235202448a541a0b164c2775
R-Eben <eben.radocchia@gmail.com>
2026-09-15 14:28:52 -0400

    Added triangle wave Co-authored-by: ColdenB <ColdenB@users.noreply.github.com>

--- Lab 1/lab1.py
+++ Lab 1/lab1.py
@@ -1,9 +1,48 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-print("Hello, EE 3150!")
-v = np.array([1.0, 2.0, 3.0])
-print("v^2 =", v**2)
-# prints to the console
-# a numpy array
-# vectorized: applies to every elemen
+t = np.arange(0.0, 6.0, 1.0 / 2000)
+# 0 to 6 s, step 0.5 ms
+x1 = np.exp(-t)
+#real exponential
+x2 = np.exp((-1 + 1j * 2) * t) #complex exponential
+x3 = np.zeros_like(t) #square
+x4 = np.zeros_like(t) #triangular
+
+T = 2
+# generate square and triangular waves below
+for i in range(len(t)):
+    rem = t[i] % T
+    
+    if rem < T/2:
+        x3[i] = 1.0
+        x4[i] = 1-2 * rem
+
+    else:
+        x3[i] =-1.0
+        x4[i] = 2 * rem - 3
+
+# plot
+plt.figure(figsize=(9, 8))
+plt.subplot(2, 2, 1)
+plt.plot(t, x1, label="exp(-t)")
+plt.xlabel("t (s)"); plt.ylabel("x1")
+plt.grid(True, alpha=0.4)
+plt.legend()
+plt.subplot(2, 2, 2)
+plt.plot(t, np.imag(x2), label="imag of exp((-1+2j)t)")
+plt.xlabel("t (s)"); plt.ylabel("x2")
+plt.grid(True, alpha=0.4)
+plt.legend()
+plt.subplot(2, 2, 3)
+plt.plot(t, x3, label="square")
+plt.xlabel("t (s)"); plt.ylabel("x3")
+plt.legend()
+plt.subplot(2, 2, 4)
+plt.grid(True, alpha=0.4)
+plt.plot(t, x4, label="triangle")
+plt.xlabel("t (s)"); plt.ylabel("x4")
+plt.legend()
+plt.grid(True, alpha=0.4)
+
+plt.savefig("signals.png", dpi=150)
